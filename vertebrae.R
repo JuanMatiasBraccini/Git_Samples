49b29e74825c70a3a282e2a9da78346a9d7a49f3
Matias Braccini <Matias.Braccini@fish.wa.gov.au>
2019-12-09 09:39:26 +0800

    updated vertebrae file. Changed data path to U drive

--- vertebrae.R
+++ vertebrae.R
@@ -13,25 +13,29 @@ library(lubridate)
 
 #Read in data
 setwd('C:\\Matias\\Analyses\\Samples\\vertebrae collection')
-dat=read.csv('Fish_processor_age_sampling.csv',stringsAsFactors = F)
+dat=read.csv('U:/Shark/Fish_processor_age_sampling.csv',stringsAsFactors = F)
+#dat=read.csv('Fish_processor_age_sampling.csv',stringsAsFactors = F)  #offline
 
 #Manipulate data
 dat=dat %>% mutate(Species=substr(Specimen.No.,1,2),
                    Date=as.POSIXct(Sampling.date,format="%d/%m/%Y"),
                    Year=year(Date),
                    Month=month(Date),
-                   Sex=ifelse(Sex=='',NA,Sex))
+                   Sex=ifelse(Sex=='',NA,Sex),
+                   Area=ifelse(grepl("Fatal Attraction",Comments),"Esperance","Albany"))%>%
+            filter(!Species=="" & !Recorder=="")
 
 #plots
 fun.plt=function(d)
 {
   d=d[order(d$Date),]
   attach(d)
-  par(mfcol=c(2,2),mar=c(2,2,1,1),oma=c(1,1,1,1),mgp=c(1,.5,0))
+  par(mfcol=c(3,2),mar=c(2,2,1,1),oma=c(1,1,1,1),mgp=c(1,.5,0))
   barplot(table(Vessel,useNA = 'ifany'),main='vessels',col=2)
   barplot(table(Date,useNA = 'ifany'),main='Date',col=2)
   barplot(table(Sex,useNA = 'ifany'),main='Sex',col=2)
   barplot(table(10*round(IDL/10),useNA = 'ifany'),main=paste('IDL (n=',nrow(d),')',sep=""),col=2)
+  barplot(table(Area,useNA = 'ifany'),main='Area',col=2)
   mtext("Frequency",2,outer=T,cex=1.5,line=-0.35)
   mtext(unique(Species),3,outer=T,cex=1.5,line=-1)
   detach(d)
